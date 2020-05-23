d63a942dc2eb91728df238d5215f442a247f650b
Olga Nazarova <headcr@bk.ru>
2020-05-23 18:53:11 +0300

    Урок 4. Примеры настройки системы логгирования через класс Program или файл конфигурации

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace WebStore
 {
@@ -12,6 +14,20 @@ namespace WebStore
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostBuilderContext, loggingBuilder)=>    //пример управления конфигурацией системы логгирования в коде (добавление провайдеров и фильтров сообщений)
+                    {
+                        //loggingBuilder.ClearProviders();  //очистить все провайдеры
+                        //loggingBuilder.AddProvider(new ConsoleLoggerProvider(/*конфигурация провайдера*/));   //добавить провайдер
+                        //loggingBuilder.AddConsole(opt => opt.IncludeScopes = true);   //консольный провайдер с включенными областями логгирования
+                        //loggingBuilder.AddEventLog(opt => opt.LogName = "WebStore");  //провайдер, записывающий информацию в системный журнал событий
+                        ////loggingBuilder.AddFilter("System", LogLevel.Warning);     //фильтр только по Warning-сообщениям пространства имен System
+                        //loggingBuilder.AddFilter((category, level) =>
+                        //{
+                        //    if (category.StartsWith("Microsoft"))   //если пространство имен Microsoft (и вложенные в него)
+                        //        return level >= LogLevel.Warning;   //будут выбираться сообщения уровня Warning и выше
+                        //    return true;
+                        //});
+                    })
                 .ConfigureWebHostDefaults(builder =>
                     builder.UseStartup<Startup>());
     }
