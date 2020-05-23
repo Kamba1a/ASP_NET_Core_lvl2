aab0e581b5d5579c395687357f3ac4b046ffb671
Olga Nazarova <headcr@bk.ru>
2020-05-23 23:03:35 +0300

    Урок 4. Добавлен проект с инфраструктурой для внедрения библиотеки log4net в стандартную систему логирования ASP.NET Core

---
+++ Common/WebStore.Logger/Log4NetExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logger
+{
+    /// <summary>класс содержащий метод расширения для ILoggerFactory</summary>
+    public static class Log4NetExtensions
+    {
+        /// <summary>
+        /// Добавление провайдера с указанным файлом конфигурации  
+        /// </summary>
+        /// <param name="Factory"></param>
+        /// <param name="ConfigurationFile">Путь к файлу конфигурации log4net</param>
+        /// <returns></returns>
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory loggerFactory, string configurationFile = "log4net.config")
+        {
+            //требуется проверка расположения указанного файла конфигурации
+            if (!Path.IsPathRooted(configurationFile)) //если путь относительный (ведет не из корневого каталога диска)
+            {
+                var assembly = Assembly.GetEntryAssembly() //получаем сборку, из которой происходит запуск приложения
+                               ?? throw new InvalidOperationException("Не найдена сборка с точкой входа в приложение");
+
+                var dir = Path.GetDirectoryName(assembly.Location) //получаем директорию расположения сборки
+                          ?? throw new InvalidOperationException("НЕ определена директория расположения стартовой сборки");
+
+                configurationFile = Path.Combine(dir, configurationFile); //корректируем путь к файлу
+            }
+
+            //добавляем провайдер с указанным файлом конфигурации
+            loggerFactory.AddProvider(new Log4NetLoggerProvider(configurationFile));
+
+            return loggerFactory;
+        }
+    }
+}
