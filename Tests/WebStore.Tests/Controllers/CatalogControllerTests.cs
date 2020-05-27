f8210212f88927c4d1588897b0d936f28dc0ce01
Olga Nazarova <headcr@bk.ru>
2020-05-27 23:05:16 +0300

    Урок 5. Тестирование CatalogController

---
+++ Tests/WebStore.Tests/Controllers/CatalogControllerTests.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Linq;
+using WebStore.Controllers;
+using WebStore.Domain;
+using WebStore.Domain.DTO.Catalog;
+using WebStore.Domain.Models;
+using WebStore.Interfaces.Services;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class CatalogControllerTests
+    {
+        [TestMethod]
+        public void ProductDetails_Returns_With_Correct_View()
+        {
+            // A-A-A = Arrange - Act - Assert
+
+            #region Arrange
+
+            const int expected_product_id = 1;
+            const decimal expected_price = 10m;
+
+            string expected_name = $"Product id {expected_product_id}";
+            string expected_brand_name = $"Brand of product {expected_product_id}";
+
+            var product_data_mock = new Mock<ICatalogData>();
+            product_data_mock
+               .Setup(p => p.GetProductById(It.IsAny<int>())) //It.IsAny сообщает Mock-объекту, какой тип данных должен передаваться в метод
+               .Returns<int>(id =>  //задаем тип и имя параметра, который будет использоваться внутри шаблона ProductDTO
+                   new ProductDTO   //создаем шаблон ProductDTO, который будет возвращать GetProductById
+                   {
+                       Id = id,
+                       Name = $"Product id {id}",
+                       ImageUrl = $"Image_id_{id}.png",
+                       Order = 1,
+                       Price = expected_price,
+                       Brand = new BrandDTO
+                       {
+                           Id = 1,
+                           Name = $"Brand of product {id}"
+                       },
+                       Section = new SectionDTO
+                       {
+                           Id = 1,
+                           Name = $"Section of product {id}"
+                       }
+                   });
+
+            var controller = new CatalogController(product_data_mock.Object);
+
+            #endregion
+
+            #region Act
+
+            var result = controller.ProductDetails(expected_product_id);
+
+            #endregion
+
+            #region Assert
+
+            var view_result = Assert.IsType<ViewResult>(result);
+
+            var model = Assert.IsAssignableFrom<ProductViewModel>(view_result.Model);
+
+            Assert.Equal(expected_product_id, model.Id);
+            Assert.Equal(expected_name, model.Name);
+            Assert.Equal(expected_price, model.Price);
+            Assert.Equal(expected_brand_name, model.BrandName);
+
+            #endregion
+        }
+
+
+        [TestMethod]
+        public void Shop_Returns_Correct_View()
+        {
+            var product_data_mock = new Mock<ICatalogData>();
+            product_data_mock
+               .Setup(p => p.GetProducts(It.IsAny<ProductFilter>()))
+               .Returns<ProductFilter>(filter => new[]  //GetProducts будет возвращать массив из двух ProductDTO
+                {
+                    new ProductDTO
+                    {
+                        Id = 1,
+                        Name = "Product 1",
+                        Order = 0,
+                        Price = 10m,
+                        ImageUrl = "Product1.png",
+                        Brand = new BrandDTO
+                        {
+                            Id = 1,
+                            Name = "Brand of product 1"
+                        } ,
+                        Section = new SectionDTO
+                        {
+                            Id = 1,
+                            Name = "Section of product 1"
+                        }
+                    },
+                    new ProductDTO
+                    {
+                        Id = 2,
+                        Name = "Product 2",
+                        Order = 0,
+                        Price = 20m,
+                        ImageUrl = "Product2.png",
+                        Brand = new BrandDTO
+                        {
+                            Id = 2,
+                            Name = "Brand of product 2"
+                        } ,
+                        Section = new SectionDTO
+                        {
+                            Id = 2,
+                            Name = "Section of product 2"
+                        }
+                    },
+                });
+
+            var controller = new CatalogController(product_data_mock.Object);
+
+            const int expected_section_id = 1;
+            const int expected_brand_id = 5;
+
+            var result = controller.Shop(expected_section_id, expected_brand_id);
+
+            var view_result = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<CatalogViewModel>(view_result.ViewData.Model);
+
+            Assert.Equal(2, model.Products.Count());
+            Assert.Equal(expected_section_id, model.SectionId);
+            Assert.Equal(expected_brand_id, model.BrandId);
+
+            Assert.Equal("Brand of product 1", model.Products.First().BrandName);
+        }
+    }
+}
