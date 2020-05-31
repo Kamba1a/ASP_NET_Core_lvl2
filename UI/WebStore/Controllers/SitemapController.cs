f973d4a134d20fedb81861b4f33d3559d849f9a0
Olga Nazarova <headcr@bk.ru>
2020-05-31 22:09:25 +0300

    Урок 6. Реализовано построение карты сайта для поисковых роботов (добавлен пакет для генерации карты сайта и отдельный контроллер для него)

---
+++ UI/WebStore/Controllers/SitemapController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleMvcSitemap;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Controllers
+{
+    //Контроллер для работы с пакетом SimpleMvcSitemap для генерации XML карты сайта (для поисковых роботов)
+    public class SitemapController : Controller
+    {
+        public IActionResult Index([FromServices] ICatalogData catalogData)
+        {
+            List<SitemapNode> nodes = new List<SitemapNode>   //составляем список статических узлов для передачи в карту сайта
+            {
+                new SitemapNode(Url.Action("Index", "Home")),
+                new SitemapNode(Url.Action("ContactUs", "Home")),
+                new SitemapNode(Url.Action("Index", "Blogs")),
+                new SitemapNode(Url.Action("BlogSingle", "Blogs")),
+                new SitemapNode(Url.Action("Shop", "Catalog")),
+                new SitemapNode(Url.Action("Index", "WebAPITest")),
+            };
+
+            //добавляем к списку все страницы секций товаров
+            nodes.AddRange(catalogData.GetSections().Select(section => new SitemapNode(Url.Action("Shop", "Catalog", new { sectionId = section.Id }))));
+
+            //добавляем к списку все страницы брендов товаров
+            foreach (var brand in catalogData.GetBrands())
+                nodes.Add(new SitemapNode(Url.Action("Shop", "Catalog", new { brandId = brand.Id })));
+
+            //добавляем к списку каждую страницу каждого товара
+            foreach (var product in catalogData.GetProducts())
+                nodes.Add(new SitemapNode(Url.Action("ProductDetails", "Catalog", new { product.Id })));
+
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes)); //передаем список провайдеру для создания карты сайта 
+        }
+    }
+}
