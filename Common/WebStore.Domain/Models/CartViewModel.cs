4a70ad9d9eb989d1d6264a3e62b12deef66afb53
Olga Nazarova <headcr@bk.ru>
2020-06-04 00:43:46 +0300

    Урок 7. Добавлен CartViewComponent, отображающий количество товаров в корзине

--- Common/WebStore.Domain/Models/CartViewModel.cs
+++ Common/WebStore.Domain/Models/CartViewModel.cs
@@ -3,9 +3,16 @@ using System.Linq;
 
 namespace WebStore.Domain.Models
 {
+    /// <summary>Модель представления корзины</summary>
     public class CartViewModel
     {
+        /// <summary>Товары в корзине</summary>
         public IEnumerable <CartItemViewModel> CartItems { get; set; }
+
+        /// <summary>Итоговая цена</summary>
         public decimal TotalPrice => CartItems.Sum(item => item.Price * item.Quantity);
+
+        /// <summary>Итоговое количество товаров</summary>
+        public int ItemsCount => CartItems.Sum(item => item.Quantity);
     }
 }
