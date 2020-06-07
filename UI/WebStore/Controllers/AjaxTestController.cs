f2d50e986e6b11c9bc3349b3ce62f5c5d987970e
Olga Nazarova <headcr@bk.ru>
2020-06-07 18:10:13 +0300

    Урок 8. Примеры использования ненавязчивого AJAX для формирования запросов к серверу

---
+++ UI/WebStore/Controllers/AjaxTestController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        public IActionResult Index() => View();
+
+        //Методы ниже занимаются одним и тем же, но по-разному возвращают результат
+
+        /// <summary>Метод возвращающей некие данные в виде Json объекта</summary>
+        /// <param name="id"></param>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> GetJSON(int? id, string msg)
+        {
+            await Task.Delay(2000);
+
+            return Json(new
+            {
+                Message = $"Response (id:{id ?? -1}): {msg ?? "<null>"}",
+                ServerTime = DateTime.Now
+            });
+        }
+
+        /// <summary>Метод возвращающий некие данные, упакованные в html-разметку</summary>
+        /// <param name="id"></param>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> GetTestView(int? id, string msg)
+        {
+            await Task.Delay(2000);
+
+            return PartialView("_Partial/_DataView", new AjaxTestViewModel
+            {
+                Id = id ?? -1,
+                Message = msg ?? "<null>",
+                ServerTime = DateTime.Now
+            });
+        }
+    }
+}
